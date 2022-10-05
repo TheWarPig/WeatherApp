fbdee5aa1047734cf8196e46b1d711e812afb660
Ido Strassberg <idostrassberg123@gmail.com>
2022-10-05 22:23:50 +0300

    Able to get input and present it

---
+++ main.py
@@ -0,0 +1,28 @@
+from configparser import ConfigParser
+import argparse
+
+def _get_api_key():
+    config = ConfigParser
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
+
+def read_user_cli_args():
+    parser = argparse.ArgumentParser(
+    description="gets weather and temperature information for a city"
+    )
+    parser.add_argument(
+    "city", nargs="+", type=str, help="enter the city name"
+    )
+    parser.add_argument(
+    "-i",
+    "--imperial",
+    action="store_true",
+    help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
+# ...
+
+if __name__ == "__main__":
+    user_args = read_user_cli_args()
+    print(user_args.city, user_args.imperial)
+
